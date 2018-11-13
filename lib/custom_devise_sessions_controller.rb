f0af8eb09f528d4cad8746c5d85d295c96da46eb
Adam D. Ruppe <destructionator@gmail.com>
2018-11-12 20:41:32 -0500

    more separation of login

--- lib/custom_devise_sessions_controller.rb
+++ lib/custom_devise_sessions_controller.rb
@@ -3,8 +3,9 @@ require 'uri'
 Devise::CasSessionsController.class_eval do
   def new
     if session[:sso]
-      service = URI.encode("#{request.base_url}/users/service", /[^\-_!~*'()a-zA-Z\d;?@&=+$,\[\]]/)
-      sso_url = session[:sso]
+      service_path = (session[:sso] == Rails.application.secrets.sso_url) ? "service_braven" : "service"
+      service = URI.encode("#{request.base_url}/users/#{service_path}", /[^\-_!~*'()a-zA-Z\d;?@&=+$,\[\]]/)
+      sso_url = session.delete(:sso)
 
       url="#{sso_url}login?service=#{service}"
       Rails.logger.info("SSO_URL: #{url}")
